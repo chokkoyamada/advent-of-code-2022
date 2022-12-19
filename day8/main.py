4fad6235c8680d36ce4ef9abc1f99ed152ea784a
Naoyuki Yamada <yamada@kirishikistudios.com>
2022-12-20 07:48:31 +0900

    day8-1

--- day8/main.py
+++ day8/main.py
@@ -1,28 +1,52 @@
+def uekara(x1, y1):
+    passed = True
+    for x in range(0, x1, 1):
+        if table[x][y1] >= table[x1][y1]:
+            return False
+    return passed
+
+
+def sitakara(x1, y1):
+    passed = True
+    for x in range(cell_length - 1, x1, -1):
+        if table[x][y1] >= table[x1][y1]:
+            return False
+    return passed
+
+
+def hidarikara(x1, y1):
+    passed = True
+    for y in range(0, y1, 1):
+        if table[x1][y] >= table[x1][y1]:
+            return False
+    return passed
+
+
+def migikara(x1, y1):
+    passed = True
+    for y in range(cell_length - 1, y1, -1):
+        if table[x1][y] >= table[x1][y1]:
+            return False
+    return passed
+
+
 table = []
 
-# 99x99のマス目にday8/inputs.txtの内容を代入する
 with open("day8/inputs.txt") as f:
-    # with open("day8/demo_inputs.txt") as f:
     for line in f:
-        table.append(list(line.strip()))
+        table.append([int(x) for x in list(line.strip())])
+cell_length = len(table[0])
 
 score = 0
-for x1 in range(0, 99, 1):
-    for y1 in range(0, 99, 1):
-        passed = True
-        for x in range(0, x1, 1):
-            if table[x][y1] > table[x1][y1]:
-                passed = False
-        for x in range(99 - 1, x1, -1):
-            if table[x][y1] > table[x1][y1]:
-                passed = False
-        for y in range(0, y1, 1):
-            if table[x1][y] > table[x1][y1]:
-                passed = False
-        for y in range(99 - 1, y1, -1):
-            if table[x1][y] > table[x1][y1]:
-                passed = False
-        if passed == True:
+for x1 in range(0, cell_length, 1):
+    for y1 in range(0, cell_length, 1):
+        print(table[x1][y1])
+        if (
+            uekara(x1, y1) == True
+            or sitakara(x1, y1) == True
+            or hidarikara(x1, y1) == True
+            or migikara(x1, y1) == True
+        ):
             score += 1
 
-print(score + 99 + 99 + 97 + 97)
+print(score)
